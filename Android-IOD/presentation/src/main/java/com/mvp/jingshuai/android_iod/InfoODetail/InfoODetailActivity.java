fbb28ff9a1b4aafabc6818ce06644ca970cddac3
jingshuai.zhang <jingshuai.zhang@ericsson.com>
2017-02-09 17:39:02 +0800

    add back button from detailed view

--- Android-IOD/presentation/src/main/java/com/mvp/jingshuai/android_iod/InfoODetail/InfoODetailActivity.java
+++ Android-IOD/presentation/src/main/java/com/mvp/jingshuai/android_iod/InfoODetail/InfoODetailActivity.java
@@ -2,6 +2,7 @@ package com.mvp.jingshuai.android_iod.InfoODetail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.mvp.jingshuai.android_iod.R;
 
@@ -14,6 +15,14 @@ public class InfoODetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getSupportActionBar() != null) {
+            // 默认左上角按钮可以点击
+            getSupportActionBar().setHomeButtonEnabled(true);
+            // 默认显示左上角返回按钮
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         setContentView(R.layout.activity_info_odetail);
 
         String taskId = getIntent().getStringExtra(EXTRA_TASK_ID);
@@ -33,6 +42,22 @@ public class InfoODetailActivity extends AppCompatActivity {
                 taskId,
                 mInfoODetailFragment);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        // 添加返回过渡动画.
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
 
